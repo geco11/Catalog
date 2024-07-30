0a256f73b9e54037f1d05d16aa878cbe90fd5496
Dilena Koplande <koplande@yahoo.com>
2024-07-30 12:28:55 -0500

    nybt

--- Catalog/Coin.h
+++ Catalog/Coin.h
@@ -2,9 +2,22 @@
 #include<iostream>
 class Coin
 {
-	float price;
-	int year;
-	unsigned char quantity;
+	char mintmark=-1;
+	int mintage=-1;
+	int year=-1;
+	std::string country="";
+	std::string collection="";
+	std::string name="";
+	bool IsMagnetic;
+	float weight=-1;
+	int diameter=-1;
+	int thickness=-1;
+	char condition = -1;
+	int quantity = -1;
+	std::string shape="";
+	float price=-1;
+	int nominal = -1;
+	float priceOfPurchase=-1;
 	std::string material;
 };
 
